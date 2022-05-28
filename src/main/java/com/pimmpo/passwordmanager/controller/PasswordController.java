e78eaf4b8a608ac680ecd08411f714e18af580fd
mavrin <mavrin@tochka.com>
2022-05-28 13:41:17 +0300

    added password generate api

---
+++ src/main/java/com/pimmpo/passwordmanager/controller/PasswordController.java
@@ -0,0 +1,52 @@
+package com.pimmpo.passwordmanager.controller;
+
+import org.passay.CharacterData;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pimmpo.passwordmanager.response.MessageResponse;
+
+/**
+ * @author mavrin
+ * @created 28.05.2022
+ */
+@RestController
+public class PasswordController {
+    
+    @GetMapping("/password/{length}")
+    public MessageResponse generatePassword(@PathVariable int length) {
+        PasswordGenerator gen = new PasswordGenerator();
+        CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+        CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+        lowerCaseRule.setNumberOfCharacters(2);
+
+        CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+        CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+        upperCaseRule.setNumberOfCharacters(2);
+
+        CharacterData digitChars = EnglishCharacterData.Digit;
+        CharacterRule digitRule = new CharacterRule(digitChars);
+        digitRule.setNumberOfCharacters(2);
+
+        CharacterData specialChars = new CharacterData() {
+            public String getErrorCode() {
+                return "ERROR_CODE";
+            }
+
+            public String getCharacters() {
+                return "!@#$%^&*()_+";
+            }
+        };
+        CharacterRule splCharRule = new CharacterRule(specialChars);
+        splCharRule.setNumberOfCharacters(2);
+
+        String password = gen.generatePassword(length, splCharRule, lowerCaseRule,
+                upperCaseRule, digitRule);
+        
+        return new MessageResponse(password);
+    }
+}
