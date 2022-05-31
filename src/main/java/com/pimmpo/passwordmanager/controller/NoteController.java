8a74cd0d1e05e59c423d492b94982bf1e8372401
mavrin <mavrin@tochka.com>
2022-06-01 00:34:24 +0300

    add new endpoint

--- src/main/java/com/pimmpo/passwordmanager/controller/NoteController.java
+++ src/main/java/com/pimmpo/passwordmanager/controller/NoteController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,5 +57,10 @@ public class NoteController {
     @PatchMapping()
     public Note updateNote(@RequestBody UpdateNoteRequest updateNoteRequest) {
         return noteService.updateNote(updateNoteRequest);
+    }
+    
+    @GetMapping("/{noteId}")
+    public Note getById(@PathVariable long noteId) {
+        return noteService.getNoteById(new GetNoteRequest(noteId));
     }
 }
